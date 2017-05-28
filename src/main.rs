f93f4ac888e5e449c898baabcd2e6ff3d89544bc
Christoffer Hjalmarsson <b.christoffer.hjalmarsson@gmail.com>
2017-05-28 18:35:51 +0200

    add libraries for dealing with real time

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 #[macro_use]
 extern crate clap;
+extern crate chrono;
+extern crate time;
+
+use time::Duration;
+// use chrono::{DateTime, UTC};
+use chrono::prelude::*;
 
 use std::env;
 use std::path::{Path, PathBuf};
@@ -23,10 +29,36 @@ fn find_db_path() -> PathBuf {
     path
 }
 
+struct activity_data<'a> {
+    activity: &'a str,
+    date_range: (DateTime<UTC>, DateTime<UTC>),
+}
+
+fn create_activity(hours: i64, activity: &str) -> activity_data {
+    let duration = Duration::hours(hours);
+    let end = UTC::now();
+    let start = end - duration;
+
+    activity_data {
+        activity,
+        date_range: (start, end),
+    }
+}
+
+#[test]
+fn test_create_activity() {
+    assert_eq!(create_activity(1, "test").activity, "test");
+    let activity = create_activity(1, "test");
+    assert_eq!(activity.date_range.1.signed_duration_since(activity.date_range.0),
+               Duration::hours(1));
+}
+
 fn main() {
     let matches = parse_cmd_matches();
     let db_path = find_db_path();
 
-    println!("activity: {}", matches.value_of("ACTIVITY").unwrap());
+    let activity = create_activity(1, matches.value_of("ACTIVITY").unwrap());
+
+    println!("activity: {}", activity.activity);
     println!("path: {}", db_path.display());
 }
